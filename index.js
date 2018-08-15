f3a17dd726546651831ccd0e20b5f9f98cd93be8
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-15 18:43:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  const lis = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  
 }
